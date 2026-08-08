c2544d3b76edede332a1e57354094b5ce2027ba5
ducminh11102012 <ducminh@mail.dangminhhoa.edu.vn>
2026-08-08 10:28:48 +0700

    feat(android): expose emulator wizard and matrix execution in UI

--- linux-app/mobilelab-android/src/MainWindow.h
+++ linux-app/mobilelab-android/src/MainWindow.h
@@ -7,10 +7,13 @@
 #include <QTextEdit>
 #include <QProgressBar>
 #include <QTimer>
-class AndroidRuntime; class ResourceScheduler; class ApiServer;
+class AndroidRuntime; class AndroidEmulator; class ResourceScheduler; class ApiServer; class MatrixExecutor; class ArtifactCollector;
 class MainWindow final:public QMainWindow{
  Q_OBJECT
 public:explicit MainWindow(QWidget*=nullptr);
-private:QWidget*makeSidebar();QWidget*makeTopBar();QWidget*makeDashboard();QWidget*makeDevices();QWidget*makeMatrix();QWidget*makeConsole();QWidget*makeInspector();void selectPage(int);
- QStackedWidget*pages_=nullptr;QListWidget*deviceList_=nullptr;QTableWidget*matrix_=nullptr;QTextEdit*console_=nullptr;QLabel*connection_=nullptr;QLabel*active_=nullptr;QProgressBar*cpu_=nullptr;QProgressBar*memory_=nullptr;QTimer*pollTimer_=nullptr;AndroidRuntime*runtime_=nullptr;ResourceScheduler*scheduler_=nullptr;ApiServer*api_=nullptr;
+private:
+ QWidget*makeSidebar(); QWidget*makeTopBar(); QWidget*makeDashboard(); QWidget*makeDevices(); QWidget*makeMatrix(); QWidget*makeConsole(); QWidget*makeInspector();
+ void selectPage(int); void refreshUi(); void showAvdWizard(); void runMatrix();
+ QStackedWidget*pages_=nullptr; QListWidget*deviceList_=nullptr; QTableWidget*matrix_=nullptr; QTextEdit*console_=nullptr; QLabel*connection_=nullptr; QLabel*active_=nullptr; QProgressBar*cpu_=nullptr; QProgressBar*memory_=nullptr; QTimer*pollTimer_=nullptr;
+ AndroidRuntime*runtime_=nullptr; AndroidEmulator*emulator_=nullptr; ResourceScheduler*scheduler_=nullptr; ApiServer*api_=nullptr; ArtifactCollector*artifacts_=nullptr; MatrixExecutor*matrixExecutor_=nullptr;
 };
