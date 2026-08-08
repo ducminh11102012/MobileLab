4f414d9460c36c04e464dc3ceb9037f016b9d7bd
ducminh11102012 <ducminh@mail.dangminhhoa.edu.vn>
2026-08-08 10:20:13 +0700

    Add local REST API server interface

---
+++ linux-app/mobilelab-android/src/core/ApiServer.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <QObject>
+#include <QTcpServer>
+#include <QJsonObject>
+class AndroidRuntime; class ResourceScheduler;
+class ApiServer:public QObject{
+ Q_OBJECT
+public:ApiServer(AndroidRuntime*,ResourceScheduler*,QObject*=nullptr);bool listen(quint16 port=4000);quint16 port()const{return m_server.serverPort();}
+signals:void logMessage(const QString&);
+private slots:void incoming();
+private:QByteArray response(int code,const QJsonObject&body)const;void handle(QTcpSocket*,const QByteArray&);
+ QTcpServer m_server;AndroidRuntime*m_runtime;ResourceScheduler*m_scheduler;
+};
